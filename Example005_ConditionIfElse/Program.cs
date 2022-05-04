cab87baf432d526d119cfe20b74801a578c6e4ff
Igor Popov <popov_i_v@mail.ru>
2022-05-04 10:14:01 +0300

    add Example 005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ura! Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
